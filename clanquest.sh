ef83641b4dfbfea541ffcb521dda8a01c2245c30
Hugo Viegas <69760051+hugoviegas@users.noreply.github.com>
2023-10-26 13:21:57 +0100

    update 3.0.16

--- clanquest.sh
+++ clanquest.sh
@@ -1,11 +1,11 @@
 checkQuest() {
   quest_id="$@"
+  clan_id
+  if [ -n "${CLD}" ]; then
   (
     w3m -cookie -o http_proxy="$PROXY" -o accept_encoding=UTF-8 -debug -dump_source "${URL}/clan/${CLD}/quest/" -o user_agent="$(shuf -n1 "$TMP"/userAgent.txt)" >"$TMP"/SRC
   ) </dev/null &>/dev/null &
   time_exit 20
-  clan_id
-  if [ -n "${CLD}" ]; then
   local click=$(grep -r -q "/clan/${CLD}/quest/(take|help|deleteHelp|end)/$quest_id" "$TMP"/SRC  | sed -n '1p')
   echo "$click"
   grep -r -q "/clan/${CLD}/quest/(take|help|deleteHelp|end)/$quest_id" "$TMP"/SRC  | sed -n '1p'
