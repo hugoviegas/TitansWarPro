6e430f27aa0336169552df5538329ebac9ea86e4
Hugo Viegas <69760051+hugoviegas@users.noreply.github.com>
2023-10-26 07:47:33 +0100

    fixing check missions

--- check.sh
+++ check.sh
@@ -5,31 +5,29 @@ check_missions() {
   ) </dev/null &>/dev/null &
   time_exit 20
   #open chests
-  if grep -o -E "/quest/openChest/(1|2)/[?]r=[0-9]+" "$TMP"/SRC; then
+  while grep -o -E "/quest/openChest/(1|2)/[?]r=[0-9]+" "$TMP"/SRC; do
     click=$(grep -o -E "/quest/openChest/(1|2)/[?]r=[0-9]+" "$TMP"/SRC)
     (
       w3m -cookie -o http_proxy="$PROXY" -o accept_encoding=UTF-8 -debug -dump_source "${URL}/quest/$click" -o user_agent="$(shuf -n1 "$TMP"/userAgent.txt)" >"$TMP"/SRC
     ) </dev/null &>/dev/null &
     time_exit 20
     printf "${GREEN_BLACK}Chest opened (✔)${COLOR_RESET}\n"
-  fi
+  done
   (
     w3m -cookie -o http_proxy="$PROXY" -o accept_encoding=UTF-8 -debug -dump_source "${URL}/quest/$click" -o user_agent="$(shuf -n1 "$TMP"/userAgent.txt)" >"$TMP"/SRC
   ) </dev/null &>/dev/null &
   time_exit 20
   #collect quests
   i=0
-  while [ $i -lt 15 ]; do
-    if grep -o -E "/quest/end/${i}[?]r=[0-9]+" "$TMP"/SRC; then
-      click=$(grep -o -E "/quest/end/${i}[?]r=[0-9]+" "$TMP"/SRC | sed -n '1p')
+  for i in {0..15} do
+    click=$(grep -o -E "/quest/end/${i}[?]r=[0-9]+" "$TMP"/SRC | sed -n '1p')
       (
         w3m -cookie -o http_proxy="$PROXY" -o accept_encoding=UTF-8 -debug -dump_source "${URL}${click}" -o user_agent="$(shuf -n1 "$TMP"/userAgent.txt)" >"$TMP"/SRC
       ) </dev/null &>/dev/null &
       time_exit 20
       printf "${GREEN_BLACK}Mission Completed (✔)${COLOR_RESET}\n"
-    fi
-    i=$((i + 1))
   done
+
   #collect rewards
   (
     w3m -cookie -o http_proxy="$PROXY" -o accept_encoding=UTF-8 -debug -dump_source "${URL}/relic/reward/" -o user_agent="$(shuf -n1 "$TMP"/userAgent.txt)" >"$TMP"/SRC
