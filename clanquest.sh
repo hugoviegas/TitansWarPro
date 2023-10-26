d064e9f6eab5c6345a1f0787bf52f82edc3515db
Hugo Viegas <69760051+hugoviegas@users.noreply.github.com>
2023-10-26 14:10:19 +0100

    update 3.0.18

--- clanquest.sh
+++ clanquest.sh
@@ -6,20 +6,19 @@ checkQuest() {
     w3m -cookie -o http_proxy="$PROXY" -o accept_encoding=UTF-8 -debug -dump_source "${URL}/clan/${CLD}/quest/" -o user_agent="$(shuf -n1 "$TMP"/userAgent.txt)" >"$TMP"/SRC
   ) </dev/null &>/dev/null &
   time_exit 20
-  local click=$(grep -r -q "/clan/${CLD}/quest/(take|help|deleteHelp|end)/$quest_id" "$TMP"/SRC  | sed -n '1p')
+  local click=$(grep -q -o -E "/clan/${CLD}/quest/(take|help|deleteHelp|end)/$quest_id+" "$TMP"/SRC  | sed -n '1p')
   echo "$click"
-  echo "$(grep -r -q "/clan/${CLD}/quest/(take|help|deleteHelp|end)/$quest_id" "$TMP"/SRC  | sed -n '1p')"
   echo "click"
   sleep 2s
   if ! echo "$click" | grep -q -o "id=${quest_id}"; then
     (
-      w3m -cookie -o http_proxy="$PROXY" -o accept_encoding=UTF-8 -debug -dump "${URL}${click}" -o user_agent="$(shuf -n1 "$TMP"/userAgent.txt)" | tail -n0
+      w3m -cookie -o http_proxy="$PROXY" -o accept_encoding=UTF-8 -debug -dump "${URL}$click" -o user_agent="$(shuf -n1 "$TMP"/userAgent.txt)" | tail -n0
     ) </dev/null &>/dev/null &
     time_exit 17
     echo " Quest $quest_id Check..."
   elif echo "$click" | grep -q -o "end"; then
   (
-      w3m -cookie -o http_proxy="$PROXY" -o accept_encoding=UTF-8 -debug -dump "${URL}${click}" -o user_agent="$(shuf -n1 "$TMP"/userAgent.txt)" | tail -n0
+      w3m -cookie -o http_proxy="$PROXY" -o accept_encoding=UTF-8 -debug -dump "${URL}$click" -o user_agent="$(shuf -n1 "$TMP"/userAgent.txt)" | tail -n0
     ) </dev/null &>/dev/null &
     time_exit 17
     echo " Quest $quest_id Completed ✅"
