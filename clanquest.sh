d1e8e70410aac0d45bc8674c098ba87ceb417512
Hugo Viegas <69760051+hugoviegas@users.noreply.github.com>
2023-10-26 12:04:48 +0100

    update 3.0.13

--- clanquest.sh
+++ clanquest.sh
@@ -1,21 +1,24 @@
 checkQuest() {
   quest_id="$@"
   (
-    w3m -cookie -o http_proxy="$PROXY" -o accept_encoding=UTF-8 -debug -dump_source "${URL}/clan/$CLD/quest/" -o user_agent="$(shuf -n1 "$TMP"/userAgent.txt)" >"$TMP"/SRC
+    w3m -cookie -o http_proxy="$PROXY" -o accept_encoding=UTF-8 -debug -dump_source "${URL}/clan/${CLD}/quest/" -o user_agent="$(shuf -n1 "$TMP"/userAgent.txt)" >"$TMP"/SRC
   ) </dev/null &>/dev/null &
   time_exit 20
   clan_id
-  if [ -n "$CLD" ]; then
+  if [ -n "${CLD}" ]; then
   #local BREAK=$(($(date +%s) + 1)) || [ "$(date +%s)" -lt "$BREAK" ]
-  while grep -q -o -E "/clan/$CLD/quest/(take|help|deleteHelp|end)/" "$TMP"/SRC ; do
+  #while grep -q -o -E "/clan/${CLD}/quest/(take|help|deleteHelp|end)/" "$TMP"/SRC ; do
   local click
-  click=$(grep -q -o -E "/clan/$CLD/quest/(take|help|deleteHelp|end)/$quest_id" "$TMP"/SRC)
+  click=$(grep -q -o -E "/clan/${CLD}/quest/(take|help|deleteHelp|end)/${quest_id}" "$TMP"/SRC)
+  if ! echo "$click" | grep -q "id=${quest_id}"; then
     (
-      w3m -cookie -o http_proxy="$PROXY" -o accept_encoding=UTF-8 -debug -dump "${URL}$click" -o user_agent="$(shuf -n1 "$TMP"/userAgent.txt)" | tail -n0
+      w3m -cookie -o http_proxy="$PROXY" -o accept_encoding=UTF-8 -debug -dump "${URL}${click}" -o user_agent="$(shuf -n1 "$TMP"/userAgent.txt)" | tail -n0
     ) </dev/null &>/dev/null &
     time_exit 17
     echo " Quest $quest_id Check ✅"
-  done
+  fi
+  #done
+    
   else
     (
       w3m -cookie -o http_proxy="$PROXY" -o accept_encoding=UTF-8 -debug -dump "$URL/clanrating/wantedToClan" -o user_agent="$(shuf -n1 "$TMP"/userAgent.txt)" | tail -n 0
