990dd99efcbad9fb0be5fcefefd3d62573d6d65c
Hugo Viegas <69760051+hugoviegas@users.noreply.github.com>
2023-10-26 11:03:50 +0100

    update 3.0.8

--- arena.sh
+++ arena.sh
@@ -80,6 +80,27 @@ arena_deleteEnd() {
     time_exit 17
   fi
 }
+completeQuest() {
+  quest_id="$@"
+  clan_id
+  if [ -n "$CLD" ]; then
+  local BREAK=$(($(date +%s) + 5))
+  while grep -q -o -E '/clan/$CLD/quest/(deleteHelp|end)/' "$TMP"/SRC || [ "$(date +%s)" -lt "$BREAK" ]; do
+  local click
+  click=$(grep -q -o -E '/clan/$CLD/quest/(deleteHelp|end)/' "$TMP"/SRC)
+    (
+      w3m -cookie -o http_proxy="$PROXY" -o accept_encoding=UTF-8 -debug -dump "${URL}'$click'$quest_id" -o user_agent="$(shuf -n1 "$TMP"/userAgent.txt)" | tail -n0
+    ) </dev/null &>/dev/null &
+    time_exit 17
+    echo " Quest $quest_id Check"
+  done
+  else
+    (
+      w3m -cookie -o http_proxy="$PROXY" -o accept_encoding=UTF-8 -debug -dump "$URL/clanrating/wantedToClan" -o user_agent="$(shuf -n1 "$TMP"/userAgent.txt)" | tail -n 0
+    ) </dev/null &>/dev/null &
+    time_exit 17
+  fi
+}
 arena_duel() {
   # arena_collFight
   # arena_fault
@@ -113,7 +134,7 @@ arena_duel() {
       w3m -cookie -o http_proxy="$PROXY" -o accept_encoding=UTF-8 -debug -dump_source "${URL}${SELL}" -o user_agent="$(shuf -n1 "$TMP"/userAgent.txt)" >"$TMP"/SRC
     ) </dev/null &>/dev/null &
     time_exit 17
-    echo " Sell all itens ✅\n"
+    echo " Sell all itens ✅"
   #arena_deleteEnd
   completeQuest 3
   completeQuest 4
