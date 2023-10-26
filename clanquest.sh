2e769ff5e1136b13efc2024d64a655ca87457841
Hugo Viegas <69760051+hugoviegas@users.noreply.github.com>
2023-10-26 16:08:15 +0100

    update 3.0.28

--- clanquest.sh
+++ clanquest.sh
@@ -6,16 +6,13 @@ checkQuest() {
     w3m -cookie -o http_proxy="$PROXY" -o accept_encoding=UTF-8 -debug -dump_source "${URL}/clan/${CLD}/quest/" -o user_agent="$(shuf -n1 "$TMP"/userAgent.txt)" >"$TMP"/SRC
   ) </dev/null &>/dev/null &
   time_exit 20
-  click=$(grep -o "/quest/\(take\|help\|deleteHelp\|end\)/$*/[?]" "$TMP"/SRC | sed -n '1p')
-  link=${click#"$*/[?]"}
-  echo "$link"
+  click=$(grep -o "/quest/\(take\|help\|deleteHelp\|end\)/$*/[?]r[=][0-9]\{8\}" "$TMP"/SRC | sed -n '1p')
+
   echo "$click"
-  echo "$*"
-  sleep 2s
     (
       w3m -cookie -o http_proxy="$PROXY" -o accept_encoding=UTF-8 -debug -dump "${URL}$click" -o user_agent="$(shuf -n1 "$TMP"/userAgent.txt)" | tail -n0
     ) </dev/null &>/dev/null &
-    time_exit 17
+    time_exit 20
     echo " Quest $quest_id Check..."
   
   #done
