c84725f247f8c9954b5caa283a713fe53d9a769e
Hugo Viegas <69760051+hugoviegas@users.noreply.github.com>
2023-12-15 13:41:27 +0000

    fixing clanmissions

--- info.sh
+++ info.sh
@@ -26,7 +26,7 @@ script_slogan() {
      m=89
      author="author: Hugo Viegas"
      #collaborator="collaborator: @_hviegas"
-     versionNum=3.1.05
+     versionNum=3.1.06
      for i in $colors; do
           clear
           t=$((t - 27))
