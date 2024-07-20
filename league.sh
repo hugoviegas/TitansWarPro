a4c69850d6f46369c787f3b8394be4ca0a4503a1
Hugo Viegas <69760051+hugoviegas@users.noreply.github.com>
2024-07-20 19:19:47 -0300

    beta 3.3.15

--- league.sh
+++ league.sh
@@ -12,7 +12,7 @@ output_file="cleaned_league_file"
     echo -e "${GOLD_BLACK}League ⚔️${COLOR_RESET}"
     #for num in $(seq 5 -1 1); do
       (
-        w3m -cookie -o http_proxy=$PROXY -o accept_encoding=UTF-8 -debug -dump_source "${URL}/league/" -o user_agent="$(shuf -n1 $TMP/userAgent.txt)" >$TMP/SRC
+        w3m -cookie -o http_proxy=$PROXY -o accept_encoding=UTF-8 -debug -dump_source "${URL}/league/" -o user_agent="$(shuf -n1 $TMP/userAgent.txt)" >"$TMP"/"$input_file"
       ) </dev/null &>/dev/null &
       time_exit 20
       ATK=$(grep -o -E '/league/fight/[0-9]{1,4}/[?]r[=][0-9]+' $TMP/SRC | sed -n '4p')
@@ -20,7 +20,29 @@ output_file="cleaned_league_file"
       # Use sed to extract the relevant content
       sed -n "/<div class='old_title bold'>/,/style='display:inline-block;text-align/p" "$input_file" > "$output_file"
 
-      #!/bin/bash
+    #done
+    echo -e "${GREEN_BLACK}League ✅${COLOR_RESET}\n"
+  #fi
+}
+
+league_test() {
+  (
+        w3m -cookie -o http_proxy=$PROXY -o accept_encoding=UTF-8 -debug -dump_source "${URL}${ATK}" -o user_agent="$(shuf -n1 $TMP/userAgent.txt)" >$TMP/SRC
+      ) </dev/null &>/dev/null &
+      time_exit 20
+    (
+      w3m -cookie -o http_proxy=$PROXY -o accept_encoding=UTF-8 -debug -dump_source "${URL}/quest/" -o user_agent="$(shuf -n1 $TMP/userAgent.txt)" >$TMP/SRC
+    ) </dev/null &>/dev/null &
+    time_exit 20
+    QUEST_END=$(grep -o -E '/quest/end/7[?]r[=][0-9]+' $TMP/SRC)
+    if [ ! -z $QUEST_END ]; then
+      (
+        w3m -cookie -o http_proxy=$PROXY -o accept_encoding=UTF-8 -debug -dump_source "${URL}${QUEST_END}" -o user_agent="$(shuf -n1 $TMP/userAgent.txt)" >$TMP/SRC
+      ) </dev/null &>/dev/null &
+      time_exit 20
+    fi
+
+
 
 # Sample HTML content (you may want to read this from a file or URL)
 output_file="$TMP/league_file"
@@ -44,25 +66,4 @@ cat "$output_file"
 echo "Total: $total"
 
 
-    #done
-    echo -e "${GREEN_BLACK}League ✅${COLOR_RESET}\n"
-  #fi
-}
-
-league_test() {
-  (
-        w3m -cookie -o http_proxy=$PROXY -o accept_encoding=UTF-8 -debug -dump_source "${URL}${ATK}" -o user_agent="$(shuf -n1 $TMP/userAgent.txt)" >$TMP/SRC
-      ) </dev/null &>/dev/null &
-      time_exit 20
-    (
-      w3m -cookie -o http_proxy=$PROXY -o accept_encoding=UTF-8 -debug -dump_source "${URL}/quest/" -o user_agent="$(shuf -n1 $TMP/userAgent.txt)" >$TMP/SRC
-    ) </dev/null &>/dev/null &
-    time_exit 20
-    QUEST_END=$(grep -o -E '/quest/end/7[?]r[=][0-9]+' $TMP/SRC)
-    if [ ! -z $QUEST_END ]; then
-      (
-        w3m -cookie -o http_proxy=$PROXY -o accept_encoding=UTF-8 -debug -dump_source "${URL}${QUEST_END}" -o user_agent="$(shuf -n1 $TMP/userAgent.txt)" >$TMP/SRC
-      ) </dev/null &>/dev/null &
-      time_exit 20
-    fi
 }
