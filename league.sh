cc65e1f7c3e4a074c1eaffa9bde8a62670030dd7
Hugo Viegas <69760051+hugoviegas@users.noreply.github.com>
2024-07-20 18:33:19 -0300

    beta 3.3.11 League test

--- league.sh
+++ league.sh
@@ -3,19 +3,28 @@ league() {
     w3m -cookie -o http_proxy=$PROXY -o accept_encoding=UTF-8 -debug -dump_source "${URL}/quest/" -o user_agent="$(shuf -n1 $TMP/userAgent.txt)" >$TMP/SRC
   ) </dev/null &>/dev/null &
   time_exit 20
-  if grep -q -o -E '/league/[?]quest_t[=]quest&quest_id[=]7&qz[=][a-z0-9]+' $TMP/SRC; then
-    printf "league ...\n"
-    for num in $(seq 5 -1 1); do
+  #if grep -q -o -E '/league/[?]quest_t[=]quest&quest_id[=]7&qz[=][a-z0-9]+' $TMP/SRC; then
+    echo -e "${GOLD_BLACK}League ⚔️${COLOR_RESET}"
+    #for num in $(seq 5 -1 1); do
       (
         w3m -cookie -o http_proxy=$PROXY -o accept_encoding=UTF-8 -debug -dump_source "${URL}/league/" -o user_agent="$(shuf -n1 $TMP/userAgent.txt)" >$TMP/SRC
       ) </dev/null &>/dev/null &
       time_exit 20
       ATK=$(grep -o -E '/league/fight/[0-9]{1,4}/[?]r[=][0-9]+' $TMP/SRC | sed -n '4p')
       (
+      w3m -cookie -o http_proxy="$PROXY" -o accept_encoding=UTF-8 -dump "${URL}/mail" -o user_agent="$(shuf -n1 "$TMP/userAgent.txt")" > "$TMP/league_file"
+      ) </dev/null &>/dev/null &
+      time_exit 20
+    #done
+    echo -e "${GREEN_BLACK}League ✅${COLOR_RESET}\n"
+  #fi
+}
+
+league_test() {
+  (
         w3m -cookie -o http_proxy=$PROXY -o accept_encoding=UTF-8 -debug -dump_source "${URL}${ATK}" -o user_agent="$(shuf -n1 $TMP/userAgent.txt)" >$TMP/SRC
       ) </dev/null &>/dev/null &
       time_exit 20
-    done
     (
       w3m -cookie -o http_proxy=$PROXY -o accept_encoding=UTF-8 -debug -dump_source "${URL}/quest/" -o user_agent="$(shuf -n1 $TMP/userAgent.txt)" >$TMP/SRC
     ) </dev/null &>/dev/null &
@@ -27,6 +36,4 @@ league() {
       ) </dev/null &>/dev/null &
       time_exit 20
     fi
-    printf "league (✔)\n"
-  fi
 }
