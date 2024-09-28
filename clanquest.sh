e15eecb0137f24fb340b9fbc991895a02258dd8d
Hugo Viegas <69760051+hugoviegas@users.noreply.github.com>
2024-09-28 01:38:27 +0100

    3.6.4 Fixing clan mission

--- clanquest.sh
+++ clanquest.sh
@@ -4,21 +4,19 @@ checkQuest() {
 
   if [ -n "${CLD}" ]; then
     fetch_page "/clan/${CLD}/quest/"
-    FULL_URL="${URL}/clan/${CLD}/quest/take/${quest_id}/?r=[0-9]{8}"
-    echo "Constructed URL: ${FULL_URL}"
-    curl -I "${FULL_URL}"
-
-    response=$(curl -s -o "$TMP/SRC" -w "%{http_code}" "${FULL_URL}")
+    
+    fetch_debug_page "/clan/${CLD}/quest/" "$TMP/debug_output.txt"
+    click=$(grep -o -E "/quest/(take|help|deleteHelp|end)/$quest_id/\?r=[0-9]{8}" "$TMP"/SRC | sed -n '1p')
+    # Fetch the page
+    response=$(curl -s -o "$TMP/SRC" -w "%{http_code}" "${click}")
 
     # Check the response code
     if [ "$response" -eq 200 ]; then
-        echo "Successfully accessed the URL."
+        echo "Successfully accessed the quest."
     else
-        echo "Failed to access the URL. HTTP response code: $response"
+        echo "Failed to access the quest. HTTP response code: $response"
     fi
-
-    fetch_debug_page "/clan/${CLD}/quest/" "$TMP/debug_output.txt"
-    click=$(grep -oE "/quest/(take|help|deleteHelp|end)/$quest_id/\?r=[0-9]{8}" "$TMP"/SRC | head -n 1)
+    # find click button
     if [ -n "$click" ]; then
       echo "Found click action: $click"
     else
