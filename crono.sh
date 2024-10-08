0f7f2eec18eec1c0595f1a4d922647031aac8a15
Hugo Viegas <69760051+hugoviegas@users.noreply.github.com>
2024-10-08 22:22:50 +0100

    3.8.3 preparing the big update, fixing all bugs

--- crono.sh
+++ crono.sh
@@ -1,4 +1,4 @@
-
+# shellcheck disable=SC2317
 # shellcheck disable=SC2154
 func_crono() {
     # Get the current hour and minute
@@ -11,22 +11,6 @@ func_crono() {
 
     # Display the current URL and time
     echo -e " \033[02m$URL ⏰ $(date +%H):$(date +%M)${COLOR_RESET}"
-}
-
-list() {
-    printf "\n"
-    
-    # Verifique se os arquivos .sh existem no diretório antes de prosseguir
-    if [ -z "$(ls ~/twm/*.sh 2>/dev/null)" ]; then
-        echo "Nenhum arquivo de script encontrado no diretório ~/twm/"
-        return 1  # Saia da função se não houver arquivos
-    fi
-
-    # Listar funções definidas nos scripts
-    grep -o -E '[[:alpha:]]+?[_]?[[:alpha:]]+?[ ]?\() \{' ~/twm/*.sh | awk -F : '{ print $2 }' | awk -F \( '{ print $1 }'
-
-    # Esperar por entrada do usuário por 5 segundos
-    read -r -t 5  # Se não houver input, apenas continua
 }
 
 # Função principal 'func_cat'
@@ -43,26 +27,38 @@ func_cat() {
     cat "$TMP/msg_file"  # Display the contents of msg_file
     printf "${WHITE_BLACK}"
 
+    list() {
+        printf "\n"
+        
+        # Verifique se os arquivos .sh existem no diretório antes de prosseguir
+        if [ -z "$(ls ~/twm/*.sh 2>/dev/null)" ]; then
+            echo "Nenhum arquivo de script encontrado no diretório ~/twm/"
+            return 1  # Saia da função se não houver arquivos
+        fi
+
+        # Listar funções definidas nos scripts
+        grep -o -E '[[:alpha:]]+?[_]?[[:alpha:]]+?[ ]?\() \{' ~/twm/*.sh | awk -F : '{ print $2 }' | awk -F '{ print $1 }'
+
+        # Esperar por entrada do usuário por 5 segundos
+        read -r -t 6  # Se não houver input, apenas continua
+    }
+
+
     while true; do
         printf " \033[02mNo battles now, waiting ${i}s${COLOR_RESET}\n${WHITEb_BLACK}Enter a command or type 'list':${COLOR_RESET} \n"
-        read -r -t "$i" cmd  # Read user command with a timeout
+        read -t "$i" cmd  # Read user command with a timeout
 
         if [ "$cmd" = " " ]; then
             break  # Exit loop if only space is entered
         fi
 
         printf "\n"
-        
-        # Executar o comando inserido, ou chamar a função 'list' se for esse o comando
-        if [ "$cmd" = "list" ]; then
-            list  # Chamar a função 'list' definida fora
-        else
-            $cmd  # Executar o comando inserido pelo usuário
-        fi
-        
+        $cmd  # Execute the command entered by the user
         sleep 0.5s  # Brief pause before next iteration
+        break  # Exit after executing the command once
     done
 }
+
 
 func_sleep() {
     # Check if it's the first day of the month
