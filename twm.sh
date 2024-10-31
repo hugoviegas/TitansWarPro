9e414051200dae41ba71cb33b69c178cc0a0e56e
Hugo Viegas <69760051+hugoviegas@users.noreply.github.com>
2024-10-31 21:18:32 +0000

    3.8.37 Adding echo_t and print_t to few scripts

--- twm.sh
+++ twm.sh
@@ -17,7 +17,9 @@ script_ads() {
 }
 script_ads
 
-printf "${BLACK_CYAN}\n Starting...\n👉 Please wait...☕👴${COLOR_RESET}\n"
+#printf "${BLACK_CYAN}\n Starting...\n👉 Please wait...☕👴${COLOR_RESET}\n"
+print_t "Starting the macro wait a few seconds..." "$BLACK_CYAN" "$COLOR_RESET" "after" "☕👴"
+
 script_slogan
 sleep 1s
 #/termux
@@ -107,11 +109,11 @@ if [ -f "$HOME/twm/ur_file" ] && [ -s "$HOME/twm/ur_file" ]; then
             break &>/dev/null  # Exit the loop quietly if Enter is pressed
         fi
         
-        print_t " Hit " "$COLOR_RESET" "" "before" ""
-        print_t "[Enter]" "$GOLD_BLACK" "$COLOR_RESET" "after" ""
-        print_t " to " "$COLOR_RESET" "" "before" ""
-        print_t " reconfigure " "$GOLD_BLACK" "" "before" ""
-        print_t "${GREEN_BLACK} ${i}s${COLOR_RESET}" "$COLOR_RESET" "" "after" "\n"
+        echo_t " Hit " "$COLOR_RESET" "" "before" ""
+        echo_t "[Enter]" "$GOLD_BLACK" "$COLOR_RESET" "after" ""
+        echo_t " to " "$COLOR_RESET" "" "before" ""
+        echo_t " reconfigure " "$GOLD_BLACK" "" "before" ""
+        echo_t "${GREEN_BLACK} ${i}s${COLOR_RESET}" "$COLOR_RESET" "" "after" "\n"
 
     done
 fi
