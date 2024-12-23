c29a5899ce900a238d70c4b4c555dacc772f4daa
Hugo Viegas <69760051+hugoviegas@users.noreply.github.com>
2024-12-23 16:39:04 +0000

    Refactor `crono.sh`: enhance user command handling and improve info display functionality

--- crono.sh
+++ crono.sh
@@ -20,12 +20,19 @@ func_cat() {
     fi
 
     cat "$TMP/msg_file"
-printf "${WHITE_BLACK}"
-
-while true; do
+    printf "${WHITE_BLACK}"
+ 
+    info() {
+        printf "\n"
+        # List functions defined in scripts
+        grep -o -E '[[:alpha:]]+?[_]?[[:alpha:]]+?[ ]?\() \{' ~/twm/*.sh | awk -F\: '{ print $2 }' | awk -F \( '{ print $1 }'
+        read -r -t 30  # Wait for user input for 5 seconds
+    }
+    
+    while true; do
        
         echo_t "No battles now, waiting ${i}s" "\033[02m" "${COLOR_RESET}"
-        awk '/[[:alpha:]]+?[_]?[[:alpha:]]+?[ ]?\(\) \{/{gsub(/.*:| \(.*/, "", $0); print}' ~/twm/*.sh
+        echo_t "Enter a command or for more info enter:" "${WHITEb_BLACK}" "info${COLOR_RESET}"
 
         read -r -t "$i" cmd  # Read user command with a timeout
 
@@ -42,16 +49,13 @@ while true; do
         $cmd
 
         # Checa se o comando está na lista de comandos que não requerem break
-        for no_break_cmd in "${commands_no_break[@]}"; do
-            if [[ "$cmd" =~ $no_break_cmd ]]; then
-                # Pausa breve antes de continuar o loop
-                sleep 0.5s
-                continue 2
-            fi
-        done
+        if [[ " ${commands_no_break[@]} " =~ " ${cmd} " ]]; then
             # Pausa breve antes de continuar o loop
             sleep 0.5s
             continue
+        else
+            break  # Sai do loop para comandos que não estão na lista
+        fi
     done
 }
 
@@ -78,13 +82,6 @@ func_sleep() {
         i=60  # Set wait time to 60 seconds
         func_cat  # Call func_cat to display information
     fi
-}
-
-info() {
-    printf "\n"
-    # List functions defined in scripts
-    grep -o -E '[[:alpha:]]+?[_]?[[:alpha:]]+?[ ]?\(\) \{' ~/twm/*.sh | awk -F: '{ print $2 }' | awk -F \( '{ print $1 }'
-    read -r -t 30  # Wait for user input for 30 seconds
 }
 
 start() {
